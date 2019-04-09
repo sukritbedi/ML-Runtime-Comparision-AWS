3933dd37f703a0167dc80b0c12317c199e281194
Hari982007 <hari982007@gmail.com>
2019-04-09 23:59:19 +0530

    Hierarchical Clustering vU2.0 & Logistic Regression vU3.0

---
+++ ubuntu/Logistic_Regression/log_reg.py
@@ -0,0 +1,31 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LogisticRegression
+from sklearn import datasets
+import timeit
+
+# import some data to play with
+start =  timeit.default_timer()
+iris = datasets.load_iris()
+X = iris.data[:, :2]  # we only take the first two features.
+Y = iris.target
+
+logreg = LogisticRegression(C=1e5, solver='lbfgs', multi_class='multinomial')
+
+# Create an instance of Logistic Regression Classifier and fit the data.
+logreg.fit(X, Y)
+
+# Plot the decision boundary. For that, we will assign a color to each
+# point in the mesh [x_min, x_max]x[y_min, y_max].
+x_min, x_max = X[:, 0].min() - .5, X[:, 0].max() + .5
+y_min, y_max = X[:, 1].min() - .5, X[:, 1].max() + .5
+h = .02  # step size in the mesh
+xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+Z = logreg.predict(np.c_[xx.ravel(), yy.ravel()])
+
+# Put the result into a color plot
+Z = Z.reshape(xx.shape)
+
+stop =  timeit.default_timer()
+
+print('Time: ', stop - start,'\n')
